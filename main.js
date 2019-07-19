63e7a3847c52a386397f1f0d36e2bb3235c45967
keigando <keigando@tohmatsu.co.jp>
2019-07-19 11:49:18 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+  var elm = document.getElementById("sample");
+  elm.style.color = "blue";
+};
---
+++ test.py
@@ -0,0 +1,13 @@
+#!/Library/Frameworks/Python.framework/Versions/3.7/bin/python3
+# -*- coding: utf-8 -*-
+
+# pythonのテスト
+
+
+def add(x, y):
+    print(x+y)
+
+
+add(2, 3)
+
+add(5, 8)
